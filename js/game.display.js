25601bf1c72988ba6c2760454a60c88d8224489f
Dhinesh <sattia.dhinesh@gmail.fr>
2020-04-10 17:49:02 +0200

    etape 2

--- js/game.display.js
+++ js/game.display.js
@@ -1,55 +1,51 @@
+
 game.display = {
-  container: '',
+  container: "",
 
   layer: {
-    name: '',
-    canvas: '',
-    context2D: '',
+    name: "",
+    canvas: "",
+    context2D: "",
     posX: null,
     posY: null,
-    width: '',
-    height: '',
-    backgroundColor: '',
-    zIndex: ''
+    width: "",
+    height: "",
+    backgroundColor: "",
+    zIndex: ""
   },
 
-  createLayer: function (
-    name,
-    width,
-    height,
-    htmlContainer,
-    zIndex,
-    backgroundColor,
-    x,
-    y
-  ) {
+  createLayer: function (name, width, height, htmlContainer, zIndex, backgroundColor, x, y) {
     var layer = Object.create(this.layer);
 
-    layer.canvas = window.document.createElement('canvas');
+    layer.canvas = window.document.createElement("canvas");
 
     layer.canvas.id = name;
 
     if (backgroundColor != undefined)
       layer.canvas.style.background = backgroundColor;
 
-    layer.zIndex = zIndex;
+    layer.zIndex = zIndex
     layer.canvas.style.zIndex = zIndex;
 
-    layer.width = width;
+    layer.width = width
     layer.canvas.width = width;
 
-    layer.height = height;
+    layer.height = height
     layer.canvas.height = height;
 
-    if (x != undefined) layer.posX = x;
+    if (x != undefined)
+      layer.posX = x;
 
-    if (y != undefined) layer.posY = y;
+    if (y != undefined)
+      layer.posY = y;
 
-    layer.canvas.style.position = 'absolute';
+    layer.canvas.style.position = "absolute";
 
-    if (x != undefined) layer.canvas.style.left = x;
+    if (x != undefined)
+      layer.canvas.style.left = x;
 
-    if (y != undefined) layer.canvas.style.top = y;
+    if (y != undefined)
+      layer.canvas.style.top = y;
 
     if (htmlContainer != undefined) {
       htmlContainer.appendChild(layer.canvas);
@@ -65,5 +61,10 @@ game.display = {
   drawRectangleInLayer: function (targetLayer, width, heigth, color, x, y) {
     targetLayer.context2D.fillStyle = color;
     targetLayer.context2D.fillRect(x, y, width, heigth);
+  },
+  drawTextInLayer: function (targetLayer, text, font, color, x, y) {
+    targetLayer.context2D.font = font;
+    targetLayer.context2D.fillStyle = color;
+    targetLayer.context2D.fillText(text, x, y);
   }
-};
+}
--- js/game.js
+++ js/game.js
@@ -1,30 +1,62 @@
 var game = {
   groundWidth: 700,
   groundHeight: 400,
-  groundColor: '#000000',
+  groundColor: "#000000",
   netWidth: 6,
-  netColor: '#FFFFFF',
-
+  netColor: "#FFFFFF",
   groundLayer: null,
+  scoreLayer: null,
+  playersBallLayer: null,
+  scorePosPlayer1: 300,
+  scorePosPlayer2: 365,
+
+  ball: {
+    width: 10,
+    height: 10,
+    color: "#FFFFFF",
+    posX: 200,
+    posY: 200
+  },
+  playerOne: {
+    width: 10,
+    height: 50,
+    color: "#FFFFFF",
+    posX: 10,
+    posY: 150
+  },
+
+  playerTwo: {
+    width: 10,
+    height: 50,
+    color: "#FFFFFF",
+    posX: 600,
+    posY: 150
+  },
 
   init: function () {
-    this.groundLayer = game.display.createLayer(
-      'terrain',
-      this.groundWidth,
-      this.groundHeight,
-      undefined,
-      0,
-      '#000000',
-      0,
-      0
-    );
-    game.display.drawRectangleInLayer(
-      this.groundLayer,
-      this.netWidth,
-      this.groundHeight,
-      this.netColor,
-      this.groundWidth / 2 - this.netWidth / 2,
-      0
-    );
+    this.groundLayer = game.display.createLayer("terrain", this.groundWidth, this.groundHeight, undefined, 0, "#000000", 0, 0);
+    game.display.drawRectangleInLayer(this.groundLayer, this.netWidth, this.groundHeight, this.netColor, this.groundWidth / 2 - this.netWidth / 2, 0);
+
+    this.scoreLayer = game.display.createLayer("score", this.groundWidth, this.groundHeight, undefined, 1, undefined, 0, 0);
+    game.display.drawTextInLayer(this.scoreLayer, "SCORE", "10px Arial", "#FF0000", 10, 10);
+
+    this.playersBallLayer = game.display.createLayer("joueursetballe", this.groundWidth, this.groundHeight, undefined, 2, undefined, 0, 0);
+    game.display.drawTextInLayer(this.playersBallLayer, "JOUEURSETBALLE", "10px Arial", "#FF0000", 100, 100);
+
+    this.displayScore(0, 0);
+    this.displayBall();
+    this.displayPlayers();
+  },
+
+  displayScore: function (scorePlayer1, scorePlayer2) {
+    game.display.drawTextInLayer(this.scoreLayer, scorePlayer1, "60px Arial", "#FFFFFF", this.scorePosPlayer1, 55);
+    game.display.drawTextInLayer(this.scoreLayer, scorePlayer2, "60px Arial", "#FFFFFF", this.scorePosPlayer2, 55);
+  },
+  displayBall: function () {
+    game.display.drawRectangleInLayer(this.playersBallLayer, this.ball.width, this.ball.height, this.ball.color, this.ball.posX, this.ball.posY);
+  },
+  displayPlayers: function () {
+    game.display.drawRectangleInLayer(this.playersBallLayer, this.playerOne.width, this.playerOne.height, this.playerOne.color, this.playerOne.posX, this.playerOne.posY);
+    game.display.drawRectangleInLayer(this.playersBallLayer, this.playerTwo.width, this.playerTwo.height, this.playerTwo.color, this.playerTwo.posX, this.playerTwo.posY);
   }
 };
